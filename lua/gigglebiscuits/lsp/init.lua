5b9f2116ee06c269e86d6488564daf0ab7aab5dd
Colton Shuman <coltonbshuman@gmail.com>
2024-06-20 07:27:51 -0600

    Okay this is some cool stuff

--- lua/gigglebiscuits/lsp/init.lua
+++ lua/gigglebiscuits/lsp/init.lua
@@ -2,16 +2,25 @@ require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = { 
     "clangd",
-    "lua-language-server"
+    "lua_ls"
   }
 })
+
+local on_attach = function(_,_)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+  vim.keymap.set('n', 'gd', vim.lsp.buf.code_action, {})
+  vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_references, {})
+end
+
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").clangd.setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+  on_attach = on_attach
 }
 
---require("lspconfig").lua-language-server.setup{
---  capabilities = capabilities
---}
+require("lspconfig").lua_ls.setup{
+  capabilities = capabilities,
+  on_attach = on_attach
+}
