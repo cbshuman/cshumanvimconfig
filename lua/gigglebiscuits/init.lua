617df2ddc7fd6d11e3d9646d40297a43fca95f2d
Colton Shuman <coltonbshuman@gmail.com>
2024-06-19 20:45:04 -0600

    This is getting real nice

--- lua/gigglebiscuits/init.lua
+++ lua/gigglebiscuits/init.lua
@@ -1,35 +1,4 @@
-local function bootstrap_pckr()
-  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
-
-  if not vim.loop.fs_stat(pckr_path) then
-    vim.fn.system({
-      'git',
-      'clone',
-      "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path
-    })
-  end
-
-  vim.opt.rtp:prepend(pckr_path)
-end
-
-bootstrap_pckr()
-
-require('pckr').add {
-	'nvim-lua/plenary.nvim',
-	'nvim-telescope/telescope.nvim', tag ='0.1.6',
-  	'VonHeikemen/lsp-zero.nvim',branch = 'v3.x', requires = {
-  		{'williamboman/mason.nvim'},
-	  	{'williamboman/mason-lspconfig.nvim'},
-	  	{'neovim/nvim-lspconfig'},
-	  	{'hrsh7th/nvim-cmp'},
-    	{'hrsh7th/cmp-nvim-lsp'},
-    	{'L3MON4D3/LuaSnip'}
- 	 },
-	'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
-	'folke/tokyonight.nvim', lazy = false, priority = 100, opts ={},
-}
-
+require("gigglebiscuits.packer")
+require("gigglebiscuits.nvimtree")
 require("gigglebiscuits.treesitter")
-require("gigglebiscuits.lsp")
+require("gigglebiscuits.lsp0")
